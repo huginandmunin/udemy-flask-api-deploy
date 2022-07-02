57294369e820ac2df51f2eb3bfd5ea463e6b1ecf
George Thomas <gcthomas@uw.edu>
2022-07-02 09:29:10 -0700

    Reconfigure run to launch app from heroku

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_jwt_extended import JWTManager
 
-from db import db
 from resources.item import Item, ItemList
 from resources.user import Login, UserRegister
 from resources.store import Store, StoreList
@@ -14,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'mysecretkey'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWTManager(app)
 
 api.add_resource(Item, '/item/<string:name>')
@@ -27,8 +22,15 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
 
 
