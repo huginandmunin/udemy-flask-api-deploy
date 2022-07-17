c1b1abaa1a2123b8609024ecaaf38db00c9c244e
George Thomas <gcthomas@uw.edu>
2022-07-16 17:43:50 -0700

    Add user resource

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt_extended import JWTManager
 
 from resources.item import Item, ItemList
-from resources.user import Login, UserRegister
+from resources.user import Login, UserRegister, User
 from resources.store import Store, StoreList
 
 
@@ -26,6 +26,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(Login, '/auth')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
